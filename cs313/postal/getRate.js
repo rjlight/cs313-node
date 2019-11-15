bd8f252f50c60411cdbf358df91bfd128ffdd987
Rachel Jones <rlbluegirl101@gmail.com>
2019-11-15 14:00:31 -0700

    edit pages

--- cs313/postal/getRate.js
+++ cs313/postal/getRate.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.use(express.static('static'))
+app.use(express.static('postal'))
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
